354d4fdd3733f3a5085c51e6e79989c1f26bbd57
lotusphere <lotusphere.sea@gmail.com>
2024-07-21 16:18:41 -0700

    Add validations for User attributes

--- src/main/java/com/lotusphere/user_management/User.java
+++ src/main/java/com/lotusphere/user_management/User.java
@@ -1,11 +1,26 @@
 package com.lotusphere.user_management;
 
+import jakarta.validation.constraints.Email;
+import jakarta.validation.constraints.Min;
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.Past;
 import lombok.Data;
+
+import java.time.LocalDate;
 
 @Data
 public class User {
+    @NotBlank(message = "User name cannot be blank")
     private String name;
+
+    @Min(value = 1, message = "Age cannot be less than 1")
     private int age;
+
+    @Email(message = "E-mail format is not correct")
+    private String email;
+
+    @Past(message = "Birthday must be a past date")
+    private LocalDate birthday;
 }
 
 
