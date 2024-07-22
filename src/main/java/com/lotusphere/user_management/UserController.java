ae6edca5d22f2b2e75cf01a88e9c661476cc25a5
lotusphere <lotusphere.sea@gmail.com>
2024-07-21 22:17:51 -0700

    Add UserController

--- src/main/java/com/lotusphere/user_management/UserController.java
+++ src/main/java/com/lotusphere/user_management/UserController.java
@@ -1,2 +1,7 @@
-package com.lotusphere.user_management;public class UserController {
+package com.lotusphere.user_management;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+
+public class UserController {
 }
