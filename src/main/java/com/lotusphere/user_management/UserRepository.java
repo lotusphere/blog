fa1f7256b627a05653262f34ef9d5fd22dd43154
lotusphere <lotusphere.sea@gmail.com>
2024-07-22 18:56:30 -0700

    Add persistence layer interface

--- src/main/java/com/lotusphere/user_management/UserRepository.java
+++ src/main/java/com/lotusphere/user_management/UserRepository.java
@@ -3,5 +3,6 @@ package com.lotusphere.user_management;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 public interface UserRepository extends JpaRepository<User, Integer> {
-
+    // Create a persistence layer interface, defining an interface that abstracts the data access operations (CRUD operations) for your entities.
+    // This helps in separating the data access logic from the business logic, promoting a clean architecture and easier testing.
 }
