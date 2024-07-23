af0ce0399e29bd41e1416f6c9900b5d715339ebd
lotusphere <lotusphere.sea@gmail.com>
2024-07-22 22:54:04 -0700

    Add BaseEntity

--- src/main/java/com/lotusphere/user_management/BaseEntity.java
+++ src/main/java/com/lotusphere/user_management/BaseEntity.java
@@ -1,2 +1,32 @@
-package com.lotusphere.user_management;public class BaseEntity {
+package com.lotusphere.user_management;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.EntityListeners;
+import jakarta.persistence.MappedSuperclass;
+import lombok.Data;
+import org.springframework.data.annotation.CreatedBy;
+import org.springframework.data.annotation.CreatedDate;
+import org.springframework.data.annotation.LastModifiedBy;
+import org.springframework.data.annotation.LastModifiedDate;
+import org.springframework.data.jpa.domain.support.AuditingEntityListener;
+
+import java.time.LocalDateTime;
+
+@Data
+@MappedSuperclass
+@EntityListeners(AuditingEntityListener.class)
+public abstract class BaseEntity {
+
+    @CreatedBy
+    @Column(updatable = false)
+    private String creator;
+
+    @LastModifiedBy
+    private String modifier;
+
+    @CreatedDate
+    private LocalDateTime createdAt;
+
+    @LastModifiedDate
+    private LocalDateTime updatedAt;
 }
