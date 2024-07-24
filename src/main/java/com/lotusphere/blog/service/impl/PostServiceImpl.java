0fea95a7b1b6f8c66696aed1fdb043d4cfccd403
lotusphere <lotusphere.sea@gmail.com>
2024-07-23 23:48:50 -0700

    Add PostServiceImpl

---
+++ src/main/java/com/lotusphere/blog/service/impl/PostServiceImpl.java
@@ -0,0 +1,11 @@
+package com.lotusphere.blog.service.impl;
+
+import com.lotusphere.blog.payload.PostDto;
+import com.lotusphere.blog.service.PostService;
+
+public class PostServiceImpl implements PostService {
+    @Override
+    public PostDto createPost(PostDto postDto) {
+        return null;
+    }
+}
