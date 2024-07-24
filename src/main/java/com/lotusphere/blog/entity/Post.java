337f376601fd7e94af2f8bd5eedc32c3785934bb
lotusphere <lotusphere.sea@gmail.com>
2024-07-23 18:11:01 -0700

    Add Post entity

---
+++ src/main/java/com/lotusphere/blog/entity/Post.java
@@ -0,0 +1,23 @@
+package com.lotusphere.blog.entity;
+
+import jakarta.persistence.*;
+import lombok.Data;
+
+@Data
+@Entity
+@Table(name = "posts", uniqueConstraints = {@UniqueConstraint(columnNames = {"title"})})
+public class Post {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(nullable = false)
+    private String title;
+
+    @Column(nullable = false)
+    private String description;
+
+    @Column(nullable = false)
+    private String content;
+}
