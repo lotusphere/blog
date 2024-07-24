2076ba843942ceb7c2635df2262f162653710a87
lotusphere <lotusphere.sea@gmail.com>
2024-07-23 23:37:36 -0700

    Add TODO comments

--- src/main/java/com/lotusphere/blog/exception/ResourceNotFoundException.java
+++ src/main/java/com/lotusphere/blog/exception/ResourceNotFoundException.java
@@ -5,6 +5,8 @@ import org.springframework.web.bind.annotation.ResponseStatus;
 
 @ResponseStatus(value = HttpStatus.NOT_FOUND)
 public class ResourceNotFoundException extends RuntimeException {
+
+    // TODO: add final
     private String resourceName;
     private String fieldName;
     private String fieldValue;
