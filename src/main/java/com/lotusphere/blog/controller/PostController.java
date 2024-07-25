2b656795cfc9baf5f83a18209b84195fe0d957d3
lotusphere <lotusphere.sea@gmail.com>
2024-07-24 18:20:12 -0700

    Implement getAllPosts api

--- src/main/java/com/lotusphere/blog/controller/PostController.java
+++ src/main/java/com/lotusphere/blog/controller/PostController.java
@@ -4,10 +4,9 @@ import com.lotusphere.blog.payload.PostDto;
 import com.lotusphere.blog.service.PostService;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
 
 @RestController
 @RequestMapping("/api/posts")
@@ -25,5 +24,11 @@ public class PostController {
     @PostMapping
     public ResponseEntity<PostDto> createPost(@RequestBody PostDto postDto) {
         return new ResponseEntity<>(postService.createPost(postDto), HttpStatus.CREATED);
+    }
+
+    // get all posts
+    @GetMapping
+    public List<PostDto> getAllPosts() {
+        return postService.getAllPosts();
     }
 }
