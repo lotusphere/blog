caddb43161c787050380233246e6f45eea5101e4
lotusphere <lotusphere.sea@gmail.com>
2024-07-24 22:56:38 -0700

    Implement deletePost method

--- src/main/java/com/lotusphere/blog/service/impl/PostServiceImpl.java
+++ src/main/java/com/lotusphere/blog/service/impl/PostServiceImpl.java
@@ -59,6 +59,12 @@ public class PostServiceImpl implements PostService {
         return mapToDto(updatedPost);
     }
 
+    @Override
+    public void deletePost(long id) {
+        Post post = postRepository.findById(id).orElseThrow(() -> new ResourceNotFoundException("Post", "id", id));
+        postRepository.deleteById(id);
+    }
+
     // convert Entity to Dto
     private PostDto mapToDto(Post post) {
         PostDto postDto = new PostDto();
