737110c3821608836c0059d7cecae87e64bf1211
lotusphere <lotusphere.sea@gmail.com>
2024-07-24 17:46:47 -0700

    Add get all posts method

--- src/main/java/com/lotusphere/blog/service/PostService.java
+++ src/main/java/com/lotusphere/blog/service/PostService.java
@@ -2,7 +2,10 @@ package com.lotusphere.blog.service;
 
 import com.lotusphere.blog.payload.PostDto;
 
+import java.util.List;
+
 public interface PostService {
 
     PostDto createPost(PostDto postDto);
+    List<PostDto> getAllPosts();
 }
