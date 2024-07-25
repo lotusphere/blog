da18a0678113450757805c93b87d90c0226471f5
lotusphere <lotusphere.sea@gmail.com>
2024-07-24 18:45:50 -0700

    Implement getPostById api

--- src/main/java/com/lotusphere/blog/controller/PostController.java
+++ src/main/java/com/lotusphere/blog/controller/PostController.java
@@ -3,6 +3,7 @@ package com.lotusphere.blog.controller;
 import com.lotusphere.blog.payload.PostDto;
 import com.lotusphere.blog.service.PostService;
 import org.springframework.http.HttpStatus;
+import org.springframework.http.RequestEntity;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
@@ -26,9 +27,16 @@ public class PostController {
         return new ResponseEntity<>(postService.createPost(postDto), HttpStatus.CREATED);
     }
 
+    // TODO: wrap with ResponseEntity
     // get all posts
     @GetMapping
     public List<PostDto> getAllPosts() {
         return postService.getAllPosts();
+    }
+
+    // TODO: Long or long?
+    @GetMapping("/{id}")
+    public ResponseEntity<PostDto> getPostById(@PathVariable(name = "id") long id) {
+        return ResponseEntity.ok(postService.getPostById(id));
     }
 }
