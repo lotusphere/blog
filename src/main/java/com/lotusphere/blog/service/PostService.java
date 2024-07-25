2e8f192aa0e1b268269f14f39bdecce225a91ada
lotusphere <lotusphere.sea@gmail.com>
2024-07-24 23:31:58 -0700

    Convert getAllPost with pagination

--- src/main/java/com/lotusphere/blog/service/PostService.java
+++ src/main/java/com/lotusphere/blog/service/PostService.java
@@ -7,7 +7,8 @@ import java.util.List;
 public interface PostService {
 
     PostDto createPost(PostDto postDto);
-    List<PostDto> getAllPosts();
+    // List<PostDto> getAllPosts();
+    List<PostDto> getAllPosts(int pageNumber, int pageSize);
     PostDto getPostById(long id);
     PostDto updatePost(long id, PostDto postDto);
     void deletePost(long id);
