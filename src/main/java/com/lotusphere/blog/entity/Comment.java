3ced16d83e4c9b9ae8ed4fc0af0f32058fe16524
lotusphere <lotusphere.sea@gmail.com>
2024-07-25 23:45:13 -0700

    Add annotations

--- src/main/java/com/lotusphere/blog/entity/Comment.java
+++ src/main/java/com/lotusphere/blog/entity/Comment.java
@@ -1,9 +1,23 @@
 package com.lotusphere.blog.entity;
 
+import jakarta.persistence.*;
+import lombok.Data;
+
+@Data
+@Entity
+@Table(name = "comments")
 public class Comment {
 
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
     private long id;
+
+    @Column(nullable = false)
     private String name;
+
+    @Column(nullable = false)
     private String email;
+
+    @Column(nullable = false)
     private String body;
 }
