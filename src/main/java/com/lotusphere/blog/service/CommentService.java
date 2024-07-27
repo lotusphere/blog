02538f9cbca05cdfd1019c51c1e61b88a575f85f
lotusphere <lotusphere.sea@gmail.com>
2024-07-26 22:35:55 -0700

    Add createComment method

--- src/main/java/com/lotusphere/blog/service/CommentService.java
+++ src/main/java/com/lotusphere/blog/service/CommentService.java
@@ -1,2 +1,8 @@
-package com.lotusphere.blog.service;public class CommentService {
+package com.lotusphere.blog.service;
+
+import com.lotusphere.blog.payload.CommentDto;
+
+public interface CommentService {
+
+    CommentDto createComment(long postId, CommentDto commentDto);
 }
