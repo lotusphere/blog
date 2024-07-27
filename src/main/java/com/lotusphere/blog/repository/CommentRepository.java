186253fa5cae433a15739d685d7fd907ae6bbf85
lotusphere <lotusphere.sea@gmail.com>
2024-07-26 22:58:07 -0700

    Add findByPostId in CommentRepository

--- src/main/java/com/lotusphere/blog/repository/CommentRepository.java
+++ src/main/java/com/lotusphere/blog/repository/CommentRepository.java
@@ -3,5 +3,9 @@ package com.lotusphere.blog.repository;
 import com.lotusphere.blog.entity.Comment;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.util.List;
+
 public interface CommentRepository extends JpaRepository<Comment, Long> {
+
+    List<Comment> findByPostId(long postId);
 }
