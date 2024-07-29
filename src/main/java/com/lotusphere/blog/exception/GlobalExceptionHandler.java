9dc69b3a824f8a5bfe9fe8a49f548d2e3816c355
lotusphere <lotusphere.sea@gmail.com>
2024-07-28 23:27:22 -0700

    Handle global Exception in GlobalExceptionHandler

--- src/main/java/com/lotusphere/blog/exception/GlobalExceptionHandler.java
+++ src/main/java/com/lotusphere/blog/exception/GlobalExceptionHandler.java
@@ -20,11 +20,18 @@ public class GlobalExceptionHandler {
         return new ResponseEntity<>(errorDetails, HttpStatus.NOT_FOUND);
     }
 
-    // global exceptions
     @ExceptionHandler(BlogApiException.class)
     public ResponseEntity<ErrorDetails> handleBlogApiException(BlogApiException exception,
                                                                WebRequest webRequest) {
         ErrorDetails errorDetails = new ErrorDetails(new Date(), exception.getMessage(), webRequest.getDescription(false));
         return new ResponseEntity<>(errorDetails, HttpStatus.BAD_REQUEST);
+    }
+
+    // global exceptions
+    @ExceptionHandler(Exception.class)
+    public ResponseEntity<ErrorDetails> handleGlobalException(Exception exception,
+                                                               WebRequest webRequest) {
+        ErrorDetails errorDetails = new ErrorDetails(new Date(), exception.getMessage(), webRequest.getDescription(false));
+        return new ResponseEntity<>(errorDetails, HttpStatus.INTERNAL_SERVER_ERROR);
     }
 }
