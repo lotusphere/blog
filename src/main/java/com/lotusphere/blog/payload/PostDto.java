57a635bc4bcf66daafde543b1e85cbd29860e725
lotusphere <lotusphere.sea@gmail.com>
2024-07-28 23:41:05 -0700

    Add contraints on PostDto attributes

--- src/main/java/com/lotusphere/blog/payload/PostDto.java
+++ src/main/java/com/lotusphere/blog/payload/PostDto.java
@@ -1,5 +1,7 @@
 package com.lotusphere.blog.payload;
 
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.Size;
 import lombok.Data;
 
 import java.util.Set;
@@ -7,8 +9,18 @@ import java.util.Set;
 @Data
 public class PostDto {
     private long id;
+
+    @NotBlank
+    @Size(min = 1, message = "Post title should have at least 1 characters")
     private String title;
+
+    @NotBlank
+    @Size(min = 1, message = "Post message should have at least 1 characters")
     private String description;
+
+    @NotBlank
+    @Size(min = 1, message = "Post content should have at least 1 characters")
     private String content;
+
     private Set<CommentDto> comments;
 }
