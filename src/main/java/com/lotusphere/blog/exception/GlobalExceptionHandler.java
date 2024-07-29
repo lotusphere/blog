e11c6facd57863af8547e6f6e16c0e0bd0a7b2e1
lotusphere <lotusphere.sea@gmail.com>
2024-07-28 23:21:41 -0700

    Add GlobalExceptionHandler

---
+++ src/main/java/com/lotusphere/blog/exception/GlobalExceptionHandler.java
@@ -0,0 +1,23 @@
+package com.lotusphere.blog.exception;
+
+import com.lotusphere.blog.payload.ErrorDetails;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.ControllerAdvice;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.context.request.WebRequest;
+
+import java.util.Date;
+
+@ControllerAdvice
+public class GlobalExceptionHandler {
+
+    // handle specific exceptions
+    @ExceptionHandler(ResourceNotFoundException.class)
+    public ResponseEntity<ErrorDetails> handleResourceNotFountException(ResourceNotFoundException exception, WebRequest webRequest) {
+        ErrorDetails errorDetails = new ErrorDetails(new Date(), exception.getMessage(), webRequest.getDescription(false));
+        return new ResponseEntity<>(errorDetails, HttpStatus.NOT_FOUND);
+    }
+
+    // global exceptions
+}
