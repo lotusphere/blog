70a16b1fb6d8a7d265e61da26c9077bac5e82f59
lotusphere <lotusphere.sea@gmail.com>
2024-07-31 00:24:44 -0700

    Add attributes and annotations for Role entity

--- src/main/java/com/lotusphere/blog/entity/Role.java
+++ src/main/java/com/lotusphere/blog/entity/Role.java
@@ -1,2 +1,19 @@
-package com.lotusphere.blog.entity;public class Role {
+package com.lotusphere.blog.entity;
+
+import jakarta.persistence.*;
+import lombok.Getter;
+import lombok.Setter;
+
+@Getter
+@Setter
+@Entity
+@Table(name = "roles")
+public class Role {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(nullable = false)
+    private String name;
 }
