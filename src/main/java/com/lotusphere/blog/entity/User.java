ccf58e40081df43687db0107d9444b0512937c68
lotusphere <lotusphere.sea@gmail.com>
2024-07-31 00:29:05 -0700

    Create user-role many to many mapping

--- src/main/java/com/lotusphere/blog/entity/User.java
+++ src/main/java/com/lotusphere/blog/entity/User.java
@@ -5,6 +5,8 @@ import jakarta.persistence.*;
 import lombok.Getter;
 import lombok.Setter;
 
+import java.util.Set;
+
 @Getter
 @Setter
 @Entity
@@ -23,4 +25,15 @@ public class User {
 
     @Column(nullable = false)
     private String password;
+
+    // TODO: what is fetch type?
+    // TODO: what is cascade type?
+    // TODO: @JoinTable?
+    @ManyToMany(fetch = FetchType.EAGER, cascade = CascadeType.ALL)
+    @JoinTable(
+            name = "users_roles",
+            joinColumns = @JoinColumn(name = "user_id", referencedColumnName = "id"),
+            inverseJoinColumns = @JoinColumn(name = "role_id", referencedColumnName = "id")
+    )
+    private Set<Role> roles;
 }
