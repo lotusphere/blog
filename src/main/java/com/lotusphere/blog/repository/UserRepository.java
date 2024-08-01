d68b189af4c7f84ac8dc8fe27c1ec2e195773cf5
lotusphere <lotusphere.sea@gmail.com>
2024-07-31 22:48:02 -0700

    Add methods in UserRepository

--- src/main/java/com/lotusphere/blog/repository/UserRepository.java
+++ src/main/java/com/lotusphere/blog/repository/UserRepository.java
@@ -3,5 +3,12 @@ package com.lotusphere.blog.repository;
 import com.lotusphere.blog.entity.User;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.util.Optional;
+
 public interface UserRepository extends JpaRepository<User, Long> {
+
+    Optional<User> findByEmail(String email);
+    Optional<User> findByUsername(String username);
+    Boolean existsByEmail(String email);
+    Boolean existsByUsername(String username);
 }
